2cc56da20dce81ce6354d15d0aa4e1730ef73ec4
simran bhatia <simran18@uw.edu>
2017-01-31 14:50:55 -0800

    Updated ex 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,10 +3,10 @@
 # Load the httr and jsonlite libraries for accessing data
 library("httr")
 library("jsonlite")
+library("dplyr")
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
-
 # For what years does the API have statistical data?
 response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
 body <- content(response,"text")
@@ -29,8 +29,6 @@ usa.persons <- fromJSON(content(response,"text"))
 str(usa.persons)
 
 ## And this was only 2013...
-
-
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
 query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", population_type_code = "RF")
@@ -74,4 +72,3 @@ select(usa.seekers, applied_during_year) %>%
   mutate(applied_during_year = as.numeric(applied_during_year)) %>%  # make numbers
   summarize(sum(applied_during_year))
 
-## Also note that asylum seekers are not refugees
